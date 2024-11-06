275bdef245b49fe5673bd4ddbbcfaa0d2328294a
sannonthachai.tun <sannonthachai.tun@energyabsolute.co.th>
2024-11-06 17:03:24 +0700

    fix

--- nodejs-agent/app.js
+++ nodejs-agent/app.js
@@ -1,11 +1,17 @@
 const express = require('express')
-const { default: agent } = require("skywalking-backend-js");
+const { default: agent } = require("skywalking-backend-js")
+const axios = require('axios')
 const app = express()
 const port = 3000
 agent.start()
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/golang', async (req, res) => {
+    const { data } = await axios.get('https://golang')
+    return res.json(data)
 })
 
 app.listen(port, () => {
