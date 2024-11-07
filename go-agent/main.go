61f435d7296dbdb60a050faf3d75ff623ac6e7b6
sannonthachai.tun <sannonthachai.tun@energyabsolute.co.th>
2024-11-07 10:17:46 +0700

    add endpoint golang

--- go-agent/main.go
+++ go-agent/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"time"
 
@@ -13,9 +15,27 @@ import (
 
 func main() {
 	e := echo.New()
+
 	e.GET("/", func(c echo.Context) error {
 		fmt.Printf("Method: Get, Path: '/', Date: %s", time.Now().Format("2006-01-02 15:04:05"))
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	e.GET("/nodejs", func(c echo.Context) error {
+		fmt.Printf("Method: Get, Path: '/nodejs', Date: %s", time.Now().Format("2006-01-02 15:04:05"))
+
+		resp, err := http.Get("http://nodejs:3000")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		return c.String(http.StatusOK, string(body))
+	})
+
 	e.Logger.Fatal(e.Start(":8000"))
 }
